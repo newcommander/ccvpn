66c5c1a1f45c714eae0bfd733f796ffa6e3b5828
new_commander <new_commander@163.com>
2018-02-28 23:59:30 +0800

    ..

--- web/console.js
+++ web/console.js
@@ -1,26 +1,40 @@
 $(document).ready(function()
 {
-var tmp = 'tmp';
 var header = new Vue({
     el: '#header',
     methods: {
         showMessage: function () {
-            this.$Message('message from header')
+            this.$Message('message from header');
         }
     }
 });
 var main = new Vue({
     el: '#main',
+    data: {
+        config: "NULL"
+    },
     methods: {
         showMessage: function () {
             this.$Message('message from main');
         },
         change_ca_path: function (e) {
             this.$Message('Change CA path: ' + e);
-            tmp = e;
         },
-        buttonclick: function () {
-            this.$Message('button-click');
+        update_config: function (data, status) {
+            config = $.parseJSON(data);
+            this.$refs.ca_path.currentValue = config.ca;
+            this.$refs.port.currentValue = config.port;
+            this.$Message('' + config.cmd[0]);
+        },
+        reload_click: function () {
+            var lala = '{"op":"reload"}';
+            $.post('d', lala, this.update_config);
+        },
+        reset_click: function () {
+            this.$Message('reset-click');
+        },
+        save_click: function () {
+            this.$Message('save-click');
         }
     }
 });
@@ -28,23 +42,10 @@ var footer = new Vue({
     el: '#footer',
     methods: {
         showMessage: function () {
-            this.$Message('message from footer')
+            this.$Message('message from footer');
         }
     }
 });
-$("#debug-button").click(function () {
-    var lala = '{' +
-        '"item1":"data1",' +
-        '"item2":"data2",' +
-        '"data1":[1,2,3],' +
-        '"data2":[4,5,6]' +
-    '}';
-    $(".menu").toggle();
-    main.$refs.ca_path.currentValue = "nihao";
-    $.post("d", lala, function(data,status) {
-        lala = $.parseJSON("" + data);
-        main.$Message("" + lala.keepalive);
-    });
-});
+$.post('d', '{"op":"reload"}', main.update_config);
 //$(".content").find(".content-left").css({"color":"red","border":"2px solid red"});
 });
