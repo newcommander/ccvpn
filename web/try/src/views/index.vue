2dfec06140e1c5c14848a74a104ffc5c01a0b391
new_commander <new_commander@163.com>
2018-04-09 23:19:54 +0800

    add iview-project.

---
+++ web/try/src/views/index.vue
@@ -0,0 +1,61 @@
+<style scoped>
+    .index {
+        width: 100%;
+        text-align: center;
+    }
+
+    .index h1 {
+        height: 150px;
+    }
+
+    .index h1 img {
+        height: 100%;
+    }
+
+    .index h2 {
+        color: #666;
+        margin-bottom: 200px;
+    }
+
+    .index h2 p {
+        margin: 0 0 50px;
+    }
+
+    .index .ivu-row-flex {
+        height: 100%;
+    }
+</style>
+<template>
+    <div class="index">
+        <Row type="flex" justify="center" align="middle">
+            <Col span="24">
+                <h1>
+                    <img src="https://raw.githubusercontent.com/iview/iview/master/assets/logo.png">
+                </h1>
+                <h2>
+                    <p>Welcome to your iView app!</p>
+                    <Button type="ghost" @click="handleStart">Start iView</Button>
+                </h2>
+            </Col>
+        </Row>
+        <InputNumber :max="10" :min="1" v-model="value1"></InputNumber>
+    </div>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                value1: 1
+            }
+        },
+        methods: {
+            handleStart() {
+                this.$Message.info('' + this.value1);
+                this.$Modal.info({
+                    title: 'Bravo',
+                    content: 'Now, enjoy the convenience of iView.'
+                });
+            }
+        }
+    };
+</script>
